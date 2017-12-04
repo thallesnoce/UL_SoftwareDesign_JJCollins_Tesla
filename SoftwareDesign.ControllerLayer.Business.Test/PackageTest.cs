5d181939cb5f509579870edd815172cc53420f64
hangdu1102 <hangdu1102@gmail.com>
2017-12-04 12:49:48 +0000

    Test for View Package and Manage Package

--- SoftwareDesign.ControllerLayer.Business.Test/PackageTest.cs
+++ SoftwareDesign.ControllerLayer.Business.Test/PackageTest.cs
@@ -20,6 +20,29 @@ namespace SoftwareDesign.ControllerLayer.Business.Test
         }
 
         /// <summary>
+        /// Monica and Hang Du Use Case Implementation
+        /// Test for View Package function
+        /// </summary>
+        [TestMethod]
+        public void ViewPackage_Success()
+        {
+            var package = new PackageBusinessLayer();
+            package.GetPackage(It.IsAny<int>());
+        }
+
+        /// <summary>
+        /// Monica and Hang Use Case Implementation
+        /// Test for Manage Package function
+        /// </summary>
+        [TestMethod]
+        public void ManagePackage_Success()
+        {
+            var package = new PackageBusinessLayer();
+            package.InsertPackage(It.IsAny<string>(), It.IsAny<int>(), It.IsAny<string>(), It.IsAny<int>(), It.IsAny<DateTime>(), It.IsAny<DateTime>());
+            package.EditPackage(It.IsAny<string>(), It.IsAny<int>(), It.IsAny<string>());
+            package.DeletePackage(It.IsAny<int>());
+        }
+        /// <summary>
         /// Thalles and Jessie Use Case Implementation
         /// </summary>
         [TestMethod]
--- SoftwareDesign.View/App_Scripts/Package.js
+++ SoftwareDesign.View/App_Scripts/Package.js
@@ -1,6 +1,6 @@
 var Package;
 (function (Package) {
-    var BuyPackageController = /** @class */ (function () {
+    var BuyPackageController = (function () {
         function BuyPackageController(packageId) {
             this.packageId = packageId;
             this.selectedServices = "";
